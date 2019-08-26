9be329bc24439939f3d3bcb997b997f04e8e8649
Unknown <helpu1409@gmail.com>
2019-08-25 21:17:15 -0500

    update file

--- ejemplos_21_08_2019/gpio_zero/gpio_zero_pwm_ex5.py
+++ ejemplos_21_08_2019/gpio_zero/gpio_zero_pwm_ex5.py
@@ -2,6 +2,6 @@ from gpiozero import PWMLED
 
 led_pin = PWMLED(18)
 while True:
-    duty_s = input("Enter Brightness (0 to 100):")
+    duty_s = input("Enter Brightness (0 to 100): ")
     duty = int(duty_s)/100
-    led_pin.​value = duty
+    led_pin.value = duty
